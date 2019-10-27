dc40291f16800f9ec8b1b831f34c75bd4529a9ca
dai@owalliance.org <Koenji-2>
2019-10-27 19:36:22 +0900

    Bug fix

--- dimmer.py
+++ dimmer.py
@@ -11,7 +11,7 @@ pi.pinMode(LED_PIN, pi.OUTPUT)
 pi.softPwmCreate(LED_PIN, 0, 100)
 
 while True:
-    v = readAdcValue(CH) / 10
+    v = mcp3008.readAdcValue(CH) / 10
     pi.softPwmWrite(LED_PIN, int(v))  
     time.sleep(0.1)
 
