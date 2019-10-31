a202e8623443e5f6fb6ba83f5d9c0d83e422c2f2
dai@owalliance.org <Koenji-2>
2019-10-31 12:50:40 +0900

    add ai

---
+++ face.py
@@ -0,0 +1,39 @@
+import picamera
+import picamera.array
+import cv2
+
+cascade_file = "haarcascade_frontalface_default.xml"
+
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.resolution = (320, 240)
+
+        while True:
+            # stream.arrayに映像データを格納
+            camera.capture(stream, 'bgr', use_video_port=True)
+
+            # グレースケールに変換
+            gray = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
+
+            # カスケードファイルを利用して顔の位置を見つける
+            cascade = cv2.CascadeClassifier(cascade_file)
+            face_list = cascade.detectMultiScale(gray, minSize=(100, 100))
+
+            for (x, y, w, h) in face_list:
+                print("face_position:",x, y, w, h)
+                color = (0, 0, 255)
+                pen_w = 5
+                cv2.rectangle(stream.array, (x, y), (x+w, y+h), color, thickness = pen_w)
+
+            # stream.arrayをウィンドウに表示
+            cv2.imshow('frame', stream.array)
+
+            # "q"でウィンドウを閉じる
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+            # streamをリセット
+            stream.seek(0)
+            stream.truncate()
+
+        cv2.destroyAllWindows()
